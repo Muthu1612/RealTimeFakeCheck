73df8592bcd42feac6c6e15de166f684ff7c3217
Muthu1612 <muthuu1729@gmail.com>
2026-01-19 20:18:24 -0500

    added comments in video pipeline

--- src/pipelines/video_pipeline.py
+++ src/pipelines/video_pipeline.py
@@ -2,3 +2,17 @@
 Need to implement end to end video processing pipelines.
 
 """
+
+"""
+TODO: This is prerequisite for video pipeline implementation. Create video, audio and image embeddings using
+the notebooks in src/notebooks and save them in embeddings/ folder.
+Train the fusion model using train_fusion.py script and save the trained model in artifacts/ folder.
+
+"""
+
+"""
+TODO: Load one single video file using video loader, create audio, video and 
+image embeddings using encoders and import the torch model saved in artifacts.
+Then pass the embeddings through the fusion model to get the final prediction.
+
+"""
