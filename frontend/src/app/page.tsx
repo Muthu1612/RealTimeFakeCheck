835cb80b9917e8bc8439db57bb142a9bf042b462
Muthu1612 <muthuu1729@gmail.com>
2026-02-04 02:08:48 -0500

    added nav bar and some css

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,11 @@
+import Navigation from "@/components/ui/Navigation";
 import UploadCardWrapper from "@/components/web/UploadCardWrapper";
 
 export default function Page() {
-  return <UploadCardWrapper />;
+  return (
+    <>
+  <Navigation />
+  <UploadCardWrapper />
+  </>
+  )
 }
